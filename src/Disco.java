f479a8be8875a319109d925e7dc34d9c46642bab
Roy <roybcx@pm.me>
2022-01-25 23:42:31 +0200

    -

--- src/Disco.java
+++ src/Disco.java
@@ -1,41 +1,31 @@
 public class Disco {
+    
     public static boolean disco(boolean[] a, boolean[] b, int i) {
-        if (equalArrays(a, b, 0)) {
+        if (equalArrays(a, b, 0))
             return true;
-        }
-
-		if (i == a.length) {
-			return false;
-		}
+        if (i == a.length) 
+            return false;
         press(a, i);
-
-        if (disco(a, b, i + 1)) {
+        if (disco(a, b, i + 1))
             return true;
-        }
         press(a, i);
         return disco(a, b, i + 1);
     }
 
     private static void press(boolean[] a, int i) {
-
         a[i] = !a[i];
-        if (i > 0) {
+        if (i > 0) 
             a[i - 1] = !a[i - 1];
-        }
-        if (i < a.length - 1) {
+        if (i < a.length - 1) 
             a[i + 1] = !a[i + 1];
-        }
     }
 
     public static boolean equalArrays(boolean[] a, boolean[] b, int i) {
-        if (i == a.length) {
+        if (i == a.length)
             return true;
-        }
-
-        if (a[i] != b[i]) {
+        if (a[i] != b[i])
             return false;
-        }
-
         return equalArrays(a, b, i + 1);
     }
+    
 }
